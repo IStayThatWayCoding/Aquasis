3e23b75cbb3bf313af4a01df80b159b48f598070
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-05-18 21:05:38 -0400

    Create rules.js

---
+++ commands/Embeds/rules.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'rules',
+    aliases: [''],
+    category: 'Embeds',
+    timeout: 5000,
+    description: 'Rule Embed',
+    usage: `rules`,
+    run: async (bot, message, args) => {
+        if(!message.member.permissions.has("ADMINISTRATOR")) return;
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`${message.guild.name} - Server Rules`)
+            .setThumbnail(message.guild.iconURL({ dynamic: true }))
+            .addLine("Rule 1", 'Test')
+           
+    }
+}
