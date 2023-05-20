3985b9bc381c9ddbbd6f26279b6b2e238ea16820
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-05-20 15:17:42 -0400

    image

--- commands/Embeds/rules.js
+++ commands/Embeds/rules.js
@@ -111,7 +111,7 @@ module.exports = {
         .setDescription(rule13)
         
 
-        message.channel.send(banner)
+        message.channel.send(new Discord.Attachment('../../image.png'))
         message.channel.send(rule1embed)
         message.channel.send(rule2embed)
         message.channel.send(rule3embed)
