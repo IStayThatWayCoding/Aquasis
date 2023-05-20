e5e01eedbdf83d2605b23ae6acdf798f2954de32
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-05-20 13:43:02 -0400

    Update rules.js

--- commands/Embeds/rules.js
+++ commands/Embeds/rules.js
@@ -15,7 +15,20 @@ module.exports = {
             .setTitle(`${message.guild.name} - Server Rules`)
             .setColor(colors.MUSIC)
             .setThumbnail(message.guild.iconURL({ dynamic: true }))
-            .addField("Rule 1", 'Test')
+            .addField("Rule 1", 'Be respectful. Do not argue with, harass or bully any member of the discord. Any form of racism, sexism, homophobia, transphobia, etc. (including derogatory terms) will get you punished.')
+            .addField("Rule 2", 'Obey all staff members. Do not argue with staff - their decisions are final. If someone is breaking the rules, __ping a staff member or admin in the channel or dm a mod instead of mini-modding__. Refrain from troll/unnecessary pinging of staff.')
+            .addField("Rule 3", 'Swearing/cursing is strongly discouraged and may result in a warn/mute, especially if done excessively. This server is meant to be PG. Slurs of **__ANY KIND__** are prohibited and will result in immediate punishment.')
+            .addField("Rule 4", '')
+            .addField("Rule 5", '')
+            .addField("Rule 6", '')
+            .addField("Rule 7", '')
+            .addField("Rule 8", '')
+            .addField("Rule 9", '')
+            .addField("Rule 10", '')
+            .addField("Rule 11", '')
+            .addField("Rule 12", '')
+            .addField("Rule 13", '')
+
         
         message.channel.send(embed)
            
