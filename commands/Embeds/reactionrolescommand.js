83b530557f8815ecc973bdb2a7a619dd355d0948
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-05-21 18:35:01 -0400

    Update reactionrolescommand.js

--- commands/Embeds/reactionrolescommand.js
+++ commands/Embeds/reactionrolescommand.js
@@ -16,7 +16,7 @@ module.exports = {
 
         const embed = new Discord.MessageEmbed()
         .setColor(colors.TRANSPARENT)
-        .setDescription('Due to the nature of how pronouns can be very diverse, Starfall Islands only offers certain pronoun roles to avoid too many cluttered roles. If your pronouns are different from these, you can select "Ask for Pronouns" or "Pronouns in Bio" so that other members can know your pronouns.\n\n1️⃣ ~ He/Him\n2️⃣ ~ She/Her\n3️⃣ ~ They/Them\n4️⃣ ~ Other/Ask Me\n5️⃣ ~ Any Pronouns\n6️⃣ ~ Pronouns in Bio\n\n<a:oasisspin:1050143931775594576> ~ Events\n🚨 ~ YT Shorts Ping\n\n🤖 ~ Updates about Aquasis (bot)\n❌ ~ React to pause XP gaining')
+        .setDescription('Due to the nature of how pronouns can be very diverse, Starfall Islands only offers certain pronoun roles to avoid too many cluttered roles. If your pronouns are different from these, you can select "Ask for Pronouns" or "Pronouns in Bio" so that other members can know your pronouns.\n\n1️⃣ ~ He/Him\n2️⃣ ~ She/Her\n3️⃣ ~ They/Them\n4️⃣ ~ It/Its\n5️⃣ ~ He/They\n6️⃣ ~ She/They\n7️⃣ ~ Neos\n8️⃣ ~ Ask Me\n9️⃣ ~ Pronouns in Bio\n\n<a:oasisspin:1050143931775594576> ~ Events\n🚨 ~ YT Shorts Ping\n\n🤖 ~ Updates about Aquasis (bot)\n❌ ~ React to pause XP gaining')
     
 
         message.channel.send({
