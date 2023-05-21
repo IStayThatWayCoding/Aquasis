d824a1bd93315c6646e825f37b9179967c08c7f7
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-05-20 23:27:14 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-const { Collection, MessageEmbed, Discord } = require('discord.js');
+const { Collection, MessageEmbed } = require('discord.js');
 const config = require('../config.json');
 const autoReply = require('../modules/autoReply');
 const autoWarn = require('../modules/autoWarn');
@@ -82,7 +82,7 @@ module.exports = async (bot, message) => {
 
     const filter = (reaction, user) => user.id == message.author.id;
 
-    const embed = new Discord.MessageEmbed()
+    const embed = new MessageEmbed()
         .setColor(colors.TRANSPARENT)
         .setDescription('🐟 ~ He/Him\n🐬 ~ She/Her\n🐳 ~ They/Them\n🌊 ~ Other/Ask Me\n💧 ~ Any Pronouns\n\n<a:oasisspin:1050143931775594576> ~ Events\n🚨 ~ YT Shorts Ping\n\n🤖 ~ Updates about Aquasis (bot)\n❌ ~ React to pause XP gaining')
 
