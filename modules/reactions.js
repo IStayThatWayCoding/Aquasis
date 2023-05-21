aaabdfc7396c7e68e4be6891ce5f85301e47272b
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-05-21 01:03:20 -0400

    df

--- modules/reactions.js
+++ modules/reactions.js
@@ -1,10 +1,16 @@
 const fetch = require('node-fetch');
 const path = require('path');
 const config = require('../config.json');
+const firstMessage = require('../../Aquasis/first-message')
 
 require('dotenv').config()
 
-module.exports = async (bot, reaction, user, message) => {
+module.exports = async (bot) => {
+
+    const channelID = '903524741791506432'
+
+    firstMessage(bot, channelID, 'hello!', [])
+    
     bot.on('messageReactionAdd', (reaction, user) => {
         console.log("ADD")
     })
