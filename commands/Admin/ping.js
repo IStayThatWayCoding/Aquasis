196cee63933b80a48a882f9b1097f6f0d36f7d19
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2023-06-21 17:57:44 -0400

    Create ping.js

---
+++ commands/Admin/ping.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'ping',
+    aliases: [''],
+    category: 'Admin',
+    timeout: 5000,
+    description: 'Returns the bot's latency',
+    usage: `>ping`,
+    run: async (bot, message, args) => {
+        if (!message.member.roles.cache.has('992191733238595644')) return message.channel.send("You must have the <@&992191733238595644> role to use this command.")
+
+        console.log(parseInt('0'));
+
+        const msg = await message.channel.send('🏓 Pinging...').then(m => m.delete({timeout: 100}));
+        
+        const embed = new Discord.MessageEmbed()
+        .setColor("#322D80")
+        .setTitle("🏓 Pong!")
+        .setDescription(`Bot Latency is **${Math.floor(msg.createdTimestamp - message.createdTimestamp)} ms** \nAPI Latency is **${Math.round(bot.ws.ping)} ms**`);
+
+        message.channel.send(embed);
+        message.delete();
+           
+    }
+}
