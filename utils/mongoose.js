8ca9624f636cf342f3ea0ad9bf562fa91bd0cf21
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2024-03-06 20:40:17 -0500

    Update mongoose.js

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 module.exports = async () => {
-    await mongoose.connect('mongodb+srv://istay:JusSmi68@oasis.zpqlqqi.mongodb.net/Data', {
+    await mongoose.connect('redacted', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
